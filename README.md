17b2dc6ec223a211ac1fc5edcea5abea740baba9
Dyuti <root@thereviewindex.com>
2019-01-29 18:54:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+To run example demo -
+npm start
+localhost:3001
+
+To build dist of demo -
+npm run build
+
+To transpile dist
+npm run transpile
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './styles.css';
+
+const MyComponent = () => (
+    <h1>Hello from My Component</h1>
+);
+export default MyComponent;
